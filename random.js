46b38081b823822d3713a1c92a95ab5a6c1ee696
gdivad <110959460+gdivad@users.noreply.github.com>
2022-08-10 12:24:43 -0700

    Update random.js

--- random.js
+++ random.js
@@ -7,16 +7,30 @@ sideOne[4] = "Partner Picks";
 sideOne[5] = "You Pick";
 
 
+var sideTwo = []
+sideTwo[0] = "Forehand";
+sideTwo[1] = "Backhand";
+sideTwo[2] = "Hammer";
+sideTwo[3] = "Roller";
+sideTwo[4] = "Weak Hand";
+sideTwo[5] = "You Pick";
+
 function roll() {
 	var randomSide = Math.floor(Math.random()*(sideOne.length));
 	document.getElementById('prandom').innerHTML = sideOne[randomSide];
+
 	document.getElementById('rollCounter').innerHTML ++;
 
 
-	var storedCount = document.getElementById('rollCounter').innerHTML;
-	var storedCount_string = JSON.stringify(storedCount);
+	//var storedCount = document.getElementById('rollCounter').innerHTML;
+	//var storedCount_string = JSON.stringify(storedCount);
 	
 }
 
+function rollTwo() {
+	var randomSideTwo = Math.floor(Math.random()*(sideTwo.length));
+	document.getElementById('randomThrow').innerHTML = sideTwo[randomSideTwo];
+	//document.getElementById('rollCounter').innerHTML ++;
+}
 
 
